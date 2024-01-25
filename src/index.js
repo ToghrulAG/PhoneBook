60b25911f4fb2b8fd7dbeb1a918e1b801a037ffd
ToghrulAG <aghalitg@gmail.com>
2024-01-25 19:42:15 +0400

    added styles, responsive

--- src/index.js
+++ src/index.js
@@ -21,15 +21,14 @@ function fillData(data) {
     <td>${element.surname}</td>
     <td>${element.tel}</td>
     <td>${element.email}</td>
-    <td>
-     <button class="edit" onclick="editRow(this)">Edit</button>
-     <button class="delete" onclick="deleteRow(this)">Delete</button>
+    <td class="operations">
+     <button class="edit" onclick="editContact('${element.name}', '${element.surname}')">Edit</button>
+     <button class="delete" onclick="deleteContact('${element.id}')">Delete</button>
     </td>
     `;
         tableBody.appendChild(bodyRow)
     });
 }
-fetchData();
 
 function createContact() {
     let addName = document.querySelector('#addName').value;
@@ -58,8 +57,41 @@ function createContact() {
 
 }
 
+async function deleteContact(userId) {
+    document.querySelector('.deleteModalContainer').classList.add('open');
+
+    document.querySelector('.approveDelete').addEventListener('click', async () => {
+        
+
+        const response = await fetch(`http://localhost:3001/users/${userId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'  
+            }
+        })
+        if(response.ok) {
+            console.log('Okay')
+        }else {
+            console.log('Not okay')
+        }
+    })
+    document.querySelector('.cancelDelete').addEventListener('click', () => {
+        document.querySelector('.deleteModalContainer').classList.remove('open');
+    })
+}
+
+function editContact(name, surName) {
+    document.querySelector('.editModalContainer').classList.add('open');
+
+    document.querySelector('.editName').value = name;
+    document.querySelector('.editSurname').value = surName;
+    
+}
+
 const submitButton = document.querySelector('#createContact');
 submitButton.addEventListener('click', createContact);
+
+fetchData();
 
 
 
